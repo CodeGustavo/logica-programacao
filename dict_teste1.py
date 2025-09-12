a86a9e94b3d91f65a6852d15a6d61c312855a871
CodeGustavo <g.mesquita031@gmail.com>
2025-09-11 22:25:29 -0300

    Update dict_teste1.py

--- dict_teste1.py
+++ dict_teste1.py
@@ -6,3 +6,10 @@ lista[campo] = valorCampo
 
 verTipo = input("Ver o tipo de lista? (S/N)").upper()
 
+
+if verTipo == "S":
+    tipoLista = type(lista)
+    print(f'A lista é do tipo: ',tipoLista)
+elif verTipo == 'N':
+    print('Você não quis ver o tipo de lista')
+print(lista)
